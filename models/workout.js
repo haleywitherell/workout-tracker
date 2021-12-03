53e35cf004d4f1f7dbd1ccbca4e5ae9748102f81
haleywitherell <hwitherell22@gmail.com>
2021-12-02 21:48:12 -0500

    models added

---
+++ models/workout.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    exercises: [
+        {
+        name: {
+            type: String,
+            required: 'Please enter the exercise name',
+        },
+        type: {
+            type: String,
+            required: 'Please enter the type of exercise',
+        },
+          weights: {
+            type: Number
+        },
+          sets: {
+            type: Number
+        },
+          reps: {
+            type: Number
+        },
+          duration: {
+            type: Number,
+            required: 'Please enter the duration in minutes',
+        },
+          distance: {
+            type: Number
+          },
+        },
+      ],
+      date:{
+          type: Date,
+          default: Date.now
+      },
+});
+
+const Workout = mongoose.model("workout", workoutSchema);
+
+module.exports = Workout;
