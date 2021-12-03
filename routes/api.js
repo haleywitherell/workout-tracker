358254f8951e5244161dc83f09a819bf00ae9fac
haleywitherell <hwitherell22@gmail.com>
2021-12-03 11:24:50 -0500

    creating routes

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,18 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+// create workout route
+router.post("/", ({ body }, res) => {
+    Workout.create(body)
+      .then(workout => {
+        res.json(workout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
 
-
-
+// create new exercise route 
 
 
 module.exports = router;
