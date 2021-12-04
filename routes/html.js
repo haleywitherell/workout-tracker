4e67a14818be7e3059b23e285974718a94d80a27
haleywitherell <hwitherell22@gmail.com>
2021-12-04 11:31:10 -0500

    html routes updates

--- routes/html.js
+++ routes/html.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const router = express.Router();
-const path = require('path');
+const Workout = require("../models/workout.js");
 
+// homepage
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 })
 
+// exercise
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
+// stats
 router.get('/stats', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/stats.html'));
 });
